9d288f82c8f8cb012fc1c00edfb3cfec8843e9bc
sgoldberg1123 <coldpizza1>
2016-09-25 21:59:02 -0500

    Initial changes/additions

---
+++ app/src/main/java/uwstout/basketballapp/NewPlayer.java
@@ -0,0 +1,16 @@
+package uwstout.basketballapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Student on 9/24/2016.
+ */
+public class NewPlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_player);
+    }
+}
